4f0e1dc79c79ed688c61bfa72e74b019047f19c5
Mclorm3 <mclorm3@gmail.com>
2023-08-07 11:02:03 +0000

    review

---
+++ app.java
@@ -0,0 +1,13 @@
+We support;
+java applications,
+.net applications, 
+NodeJS applications, 
+python applications,
+web application
+bug removed
+This is great
+developing java apps
+NodeJS applications 
+great
+This app is excellent
+This app is excellent for all users
---
+++ userMgt.java
@@ -0,0 +1,8 @@
+landmark
+mercy
+dominion
+Ayo 
+Obi
+manager
+datti
+c0d3 commit by omas31
